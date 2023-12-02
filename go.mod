278ee010ba740e781ae03702e2fc1e49749b2d40
jobb-rodriguez <jobb.rodriguez22@gmail.com>
2023-12-03 02:41:52 +0800

    Add: Setup Chi Server

--- go.mod
+++ go.mod
@@ -3,7 +3,6 @@ module github.com/jobb-rodriguez/rssfeed-go
 go 1.21.3
 
 require (
-	github.com/go-chi/chi v1.5.5 // indirect
-	github.com/go-chi/cors v1.2.1 // indirect
-	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/go-chi/chi v1.5.5
+	github.com/joho/godotenv v1.5.1
 )
--- main.go
+++ main.go
@@ -1,10 +1,11 @@
 package main
 
 import (
-	"fmt"
 	"log"
+	"net/http"
 	"os"
 
+	"github.com/go-chi/chi"
 	"github.com/joho/godotenv"
 )
 
@@ -16,5 +17,16 @@ func main() {
 		log.Fatal("PORT is not found in the environment")
 	}
 
-	fmt.Println("Port:", portString)
+	router := chi.NewRouter()
+
+	srv := &http.Server{
+		Handler: router,
+		Addr:    ":" + portString,
+	}
+
+	log.Printf("Server starting on port %v", portString)
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
