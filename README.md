4c3d47ec8d68776cb40dd05c8ac9a2392f16573f
jobb-rodriguez <jobb.rodriguez22@gmail.com>
2023-12-03 02:33:44 +0800

    Add: Set up the port with godotenv

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # rssfeed-go
+Create a local copy of dependencies with vendor.
+
+```terminal
+go mod vendor
+```
--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"os"
 
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	godotenv.Load(".env")
+
+	portString := os.Getenv("PORT")
+	if portString == "" {
+		log.Fatal("PORT is not found in the environment")
+	}
+
+	fmt.Println("Port:", portString)
 }
